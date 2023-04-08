40f4db7326f9932f7e53ea05382a7bca3668fea3
faceleft <89391656+faceleft@users.noreply.github.com>
2023-04-09 01:52:14 +0700

    Update error.h

--- ArduinoMain/error.h
+++ ArduinoMain/error.h
@@ -4,17 +4,8 @@
 #include <Arduino.h>
 #include "display.h"
 
-const char* ErrorNames[] = {
-  "Alloc Error",
-  "Pointer Error"
-  };
 
-enum Errors{
-  ALLOC_ERROR,
-  POINTER_ERROR
-};
-
-void error(Errors num){
+void error(int num){
         
     tft_print("!!!!!!!!!!!!!!", 1, 255, 0, 0);
     Serial.print("!!!!!!!!!!!!!!");
@@ -22,18 +13,8 @@ void error(Errors num){
     tft_print("ERROR: ", 0, 255, 0, 0);
     Serial.print("ERROR: ");
     
-    tft_print(ErrorNames[num], 1, 255, 0, 0);
-    Serial.println(ErrorNames[num]);
-
-    tft_print("at ", 0, 255, 0, 0);
-    Serial.print("at ");
-    
-    tft_print(String((float)millis()/1024), 0, 255, 0, 0);
-    Serial.print(String((float)millis()/1024));
-    
-    tft_print(" seconds", 0, 255, 0, 0);
-    Serial.print(" seconds");
-    
+    tft_print(num, 1, 255, 0, 0);
+    Serial.println(num);    
     for(;;);
 }
 
